6ee9679bffa0c23ece002ed5196a675101b3a78f
KelvinNguyen2h3 <kelvin04092003@gmail.com>
2024-07-14 01:52:41 +0700

    first commit

---
+++ basicCpp/Variable.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int score = 82;
+	cout << " Marks scored is " << score;
+}
---
+++ basicCpp/hello.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+//function
+/*
+int sub(int a, int b) {
+	return a - b;
+}*/
+
+int main() {
+	//hello world
+	/*
+	std::cout << "Hello World";*/
+
+	/*
+	cin - Standard Input stream
+	cout - Standard Output stream*/
+
+	//variable
+	/*
+	int age = 20;
+	cout << "My age is " << score;*/
+
+	//datatype - It's so much but It's same as C, we only note the new ones
+	//string
+	/*
+	string name = "Kelvin";
+	cout << "My name is " << name;*/
+
+	//Conditional Statements
+	//if-else
+	/*
+	int num = 10;
+	if (num > 0) {
+		cout << "Number is positive" << endl;
+	}
+	else {
+		cout << "Number is non-positive" << endl;
+	}*/
+
+	//switch-case
+	/*
+	char grade = 'A';
+	switch (grade) {
+	case 'A':
+		cout << "Excellent!" << endl;
+		break;
+	case 'B':
+		cout << "Good job!" << endl;
+		break;
+	case 'C':
+		cout << "Passing grade" << endl;
+		break;
+	default:
+		cout << "Fail" << endl;
+	}*/
+
+	//loopings
+	//for
+	/*
+	for (int i = 1; i <= 5; i++) {
+		cout << "Count: " << i << endl;
+	}*/
+	//endl = end line
+
+	//while
+	/*
+	int count = 1;
+	while (count <= 5) {
+		cout << "Count: " << count << endl;
+		++count;
+	}*/
+
+	//function
+	/*cout << sub(5.25, 3.66);*/
+
+	return 0;
+}
